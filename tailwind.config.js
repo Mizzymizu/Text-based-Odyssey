432290c2f173113b81e046ccf347b04e9858398d
angiecruz02 <angelicacruz02@hotmail.com>
2023-09-11 20:54:12 -0500

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'Raisin-black': '#181D27',
+      'Anti-flash-White': '#EBEBEB',
+      'Xanthous': '#E9B020',
+      'Battleship-gray': '#8A7E72'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
