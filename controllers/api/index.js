45351c1f6e630b63c82f4dc8828c8f697bdf73a3
SnowHeyOh <shminivan@gmail.com>
2023-09-11 20:30:50 -0500

    made login page, set up server with successful connect

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const loginRoutes = require('./loginRoutes');
 const charactersRoutes = require('./charactersRoutes');
+const signupRoutes = require('./signupRoutes');
 
 router.use('/login', loginRoutes);
-router.use('/characters', charactersRoutes);
+//router.use('/characters', charactersRoutes);
+//router.use('/signup', signupRoutes);
 
 module.exports = router;
