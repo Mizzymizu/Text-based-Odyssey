18691617ec394dd90a42298430b479730c52b372
unknown <danleviobello@gmail.com>
2023-09-13 19:58:32 -0500

    Added Specialization and Skills Models

---
+++ models/Specialization.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Specialization extends Model {}
+
+Specialization.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        skills: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'skills',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'specialization',
+    }
+);
+
+module.exports = Specialization;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const Character = require('./character');
 const User = require('./user');
+const Specialization = require('./Specialization')
+const Skills = require('./Skills')
 
 // User hasMany Characters
 // Character belongs to User
+// Character hasMany Specializations
+// Specializations hasMany Skills
 
-module.exports = { Character, User };
+module.exports = { Character, User, Specialization, Skills };
