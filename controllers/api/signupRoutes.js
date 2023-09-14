e592ed9259cb89c38d2b88dd642dddcd5c87b8a2
SnowHeyOh <shminivan@gmail.com>
2023-09-13 20:40:48 -0500

    server can now successfully signup, login, check if logged in, and log out

--- controllers/api/signupRoutes.js
+++ controllers/api/signupRoutes.js
@@ -1,8 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
-const { create } = require('../../models/User');
 
 router.post('/', async (req, res) => {
     try {
