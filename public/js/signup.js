9f8e7ed0363a0973a5689d2817baf6259ed1ffad
SnowHeyOh <shminivan@gmail.com>
2023-09-16 09:46:21 -0500

    restructured signup and login forms, adjusted homepage styling

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,57 +1,69 @@
-// Get a reference to the signup form and its elements
-const signupForm = document.querySelector(".signup-form");
-const usernameInput = document.getElementById("name-signup");
-const emailInput = document.getElementById("email-signup");
-const passwordInput = document.getElementById("password-signup");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get a reference to the signup form and its elements
+  const signupForm = document.querySelector(".signup-form");
+  const loginForm = document.querySelector(".login-form");
 
-const signUpFormHandler = async (event) => {
-  event.preventDefault();
+  const signUpFormHandler = async (event) => {
+    event.preventDefault();
 
-  // Get user input values
-  const username = usernameInput.value.trim();
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+    const usernameInput = document.getElementById("name-signup");
+    const emailInput = document.getElementById("email-signup");
+    const passwordInput = document.getElementById("password-signup");
 
-  if (username && email && password) {
-    const response = await fetch("/api/signup", {
-      method: "POST",
-      body: JSON.stringify({ username, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+    // Get user input values
+    const username = usernameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
-    if (response.ok) {
-      document.location.replace("/");
+    if (username && email && password) {
+      const response = await fetch("/api/signup", {
+        method: "POST",
+        body: JSON.stringify({ username, email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert("Failed to sign up.");
+      }
     } else {
-      alert("Failed to sign up.");
+      alert("Please fill out all fields.");
     }
-  } else {
-    alert("Please fill out all fields.");
-  }
-};
+  };
 
-const loginFormHandler = async (event) => {
-  event.preventDefault();
+  const loginFormHandler = async (event) => {
+    event.preventDefault();
 
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+    const emailInput = document.getElementById("email-login");
+    const passwordInput = document.getElementById("password-login");
 
-  if (email && password) {
-    const response = await fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
-    if (response.ok) {
-      document.location.replace("/");
+    if (email && password) {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) {
+        document.location.replace("/");
+      } else {
+        alert("Failed to log in.");
+      }
     } else {
-      alert("Failed to log in.");
+      alert("Please fill out all fields.");
     }
-  } else {
-    alert("Please fill out all fields.");
-  }
-};
+  };
 
-// Event listener for form submission
-signupForm.addEventListener("submit", signUpFormHandler);
-loginForm.addEventListener("submit", loginFormHandler);
+  
+  if (signupForm) {
+    signupForm.addEventListener("submit", signUpFormHandler);
+  }
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", loginFormHandler);
+  }
+});
