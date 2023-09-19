af3969f16fa3ff576070a5d1a80438a14aaf0fc4
unknown <danleviobello@gmail.com>
2023-09-18 21:41:11 -0500

    Heroku v8

--- models/index.js
+++ models/index.js
@@ -1,34 +1,20 @@
 const User = require('./User');
-const Specialization = require('./Specialization');
-const Skills = require('./Skills');
+// const Specialization = require('./Specialization');
+// const Skills = require('./Skills');
 const Character = require('./Character');
 
-// User hasMany Characters
-// User.associate = (models) => {
-//   User.hasMany(models.Character, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE',
-//   });
-// };
+User.hasMany(Character, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-// Character.associate = (models) => {
-//   Character.belongsTo(models.User, {
-//     foreignKey: 'user_id',
-//   });
-// };
-
-// User.hasMany(Character, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Character.belongsTo(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// } );
+Character.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+} );
 
 // Character belongs to User
 // Character hasMany Specializations
 // Specializations hasMany Skills
 
-module.exports = { Character, User, Specialization, Skills };
+module.exports = { Character, User };
